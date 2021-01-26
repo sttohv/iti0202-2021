f8bac71ddd0bc542bb67832af433e6ae16a2246c
sttohv <sttohv@ttu.ee>
2021-01-27 01:37:10 +0200

    ex00 first try

---
+++ EX/EX00Hello/src/ee/taltech/iti0202/hello/Hello.java
@@ -0,0 +1,33 @@
+package ee.taltech.iti0202.hello;
+
+/**
+ * Simple class to test whether you get feedback.
+ */
+public class Hello {
+    /**
+     * Returns a greeting for the given person.
+     * <p>
+     * If name is empty string, then returns "Hello!".
+     * Otherwise returns "Hello, [name]!"
+     *
+     * @param name Name of the person.
+     * @return Greeting
+     */
+    public static String getGreeting(String name) {
+        if (name == "")
+        {
+            return "Hello!";
+        }
+        return "Hello, " + name + "!";
+    }
+
+    /**
+     * The main method which is executed when the program is executed.
+     *
+     * @param args Arguments from command line
+     */
+    public static void main(String[] args) {
+        System.out.println(getGreeting("Java"));
+        System.out.println(getGreeting(""));
+    }
+}
