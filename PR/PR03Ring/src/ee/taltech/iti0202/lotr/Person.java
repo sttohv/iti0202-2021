4f6352d123bdd8c4e74d6497968aea10c7bc1436
sttohv <sttohv@ttu.ee>
2021-02-14 13:20:05 +0200

    pr03 first push

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -21,18 +21,26 @@ public class Person {
     }
 
     public String isSauron() {
-        if (!(ring == null)) {
+        try {
+
+
             if (name.equals("Sauron") && ring.getType() == Ring.Type.THE_ONE && ring.getMaterial() == Ring.Material.GOLD) {
                 return "Affirmative";
             } else if (name.equals("Sauron") && ring.getType() == Ring.Type.THE_ONE && !(ring.getMaterial() == Ring.Material.GOLD)) {
                 return "No, the ring is fake!";
             } else if (ring.getMaterial() == Ring.Material.GOLD && ring.getType() == Ring.Type.THE_ONE && !name.equals("Sauron")) {
                 return "No, he just stole the ring";
+            } else if (name.equals("Sauron") && !(ring.getType() == Ring.Type.THE_ONE)) {
+                return "No, but he's claiming to be";
             }
-        }else if (name.equals("Sauron") || !(ring.getType() == Ring.Type.THE_ONE)) {
-            return "No, but he's claiming to be";
+            return "No";
+        } catch (Exception e) {
+            if (name.equals("Sauron")) {
+                return "No, but he's claiming to be";
+            } else {
+                return "No";
+            }
         }
-        return "No";
     }
 
     public String getRace() {
