00f7076053c12ca7d577c78a57556e14325b3a15
sttohv <sttohv@ttu.ee>
2021-02-15 20:46:29 +0200

    pr03 first push

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -54,6 +54,7 @@ public class Book {
     }
 
     public boolean buy(Person buyer) {
+        try{
         if(bookOwner.equals(buyer)||buyer.buyBook(this)){
             return false; // ei õnnestunud - praegune omanik, kui raha pole(kontrollib buyBook),
         }
@@ -61,6 +62,9 @@ public class Book {
             Person.setPersonMoney(buyer.getMoney()-getPrice());
             setBookOwner(buyer);
             return true; //õnnestus -
+        }}
+        catch (Exception e){
+            return false;
         }
     }
 
