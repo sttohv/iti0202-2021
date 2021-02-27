0ff805dccd6f2757eaed905f60f3fcee10fe0035
sttohv <sttohv@ttu.ee>
2021-02-27 15:14:23 +0200

    Stiil lappe aga tk tehtud äkki

--- TK/TK2/src/ee/taltech/iti0202/tk2/shop/Product.java
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Product.java
@@ -25,7 +25,7 @@ public class Product {
     }
     @Override
     public String toString() {
-        if (productName.equals(null)) {
+        if (this==null && productName.equals(null)) {
             return "(" + productPrice + ")";
         } else {
             return productName + " (" + productPrice + ")";
