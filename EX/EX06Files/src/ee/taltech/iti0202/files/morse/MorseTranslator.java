c923934b14bd9ae73772c2a514460fdc127b65d6
sttohv <sttohv@ttu.ee>
2021-03-09 15:03:17 +0200

    ex06

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -80,8 +80,8 @@ public class MorseTranslator {
             if (!letter.equals(" ")) {
                 result.append(morseLetter.get(letter)).append(" ");
             } else {
-
-                result.append(" \t");
+                result.append(" ");
+                result.append("\t");
             }
         }
         return result.toString();
