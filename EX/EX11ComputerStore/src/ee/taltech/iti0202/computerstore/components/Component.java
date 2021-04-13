9e6fb223034726e2db07db3fa1975b4e87f22712
sttohv <sttohv@ttu.ee>
2021-04-13 14:01:32 +0300

    ex11 component added getter and setters

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/components/Component.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/components/Component.java
@@ -46,4 +46,52 @@ public class Component {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getPerformancePoints() {
+        return performancePoints;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setPerformancePoints(int performancePoints) {
+        this.performancePoints = performancePoints;
+    }
+
+    public void setPowerConsumption(int powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
