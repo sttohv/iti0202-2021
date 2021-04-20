0b0fbac6419fb1c767ced3350ab781666be08b8b
sttohv <sttohv@ttu.ee>
2021-04-20 20:27:44 +0300

    LX01 style fixed

--- LX/LX01Stream/src/ee/taltech/iti0202/stream/Kitten.java
+++ LX/LX01Stream/src/ee/taltech/iti0202/stream/Kitten.java
@@ -1,11 +1,20 @@
 package ee.taltech.iti0202.stream;
+
 public class Kitten {
 
     private Gender gender;
     private String name;
     private int age;
-    public enum Gender { MALE, FEMALE }
 
+    public enum Gender {MALE, FEMALE}
+
+    /**
+     * something
+     *
+     * @param name   a
+     * @param gender b
+     * @param age    c
+     */
     public Kitten(String name, Gender gender, int age) {
         this.name = name;
         this.gender = gender;
--- LX/LX01Stream/src/ee/taltech/iti0202/stream/KittenStatistics.java
+++ LX/LX01Stream/src/ee/taltech/iti0202/stream/KittenStatistics.java
@@ -1,34 +1,67 @@
 package ee.taltech.iti0202.stream;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class KittenStatistics {
 
     private List<Kitten> kittens;
 
+    /**
+     * something
+     *
+     * @param kittens kittens
+     */
     public void setKittens(List<Kitten> kittens) {
         this.kittens = kittens;
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public OptionalDouble findKittensAverageAge() {
         return kittens.stream().mapToDouble(Kitten::getAge).average();
 
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public Optional<Kitten> findOldestKitten() {
         return kittens.stream().max(Comparator.comparingInt(Kitten::getAge));
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public List<Kitten> findYoungestKittens() {
         int age = kittens.stream().min(Comparator.comparingInt(Kitten::getAge)).get().getAge();
         return kittens.stream().filter(i -> i.getAge() == age).collect(Collectors.toList());
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public List<Kitten> findKittensAccordingToGender(Kitten.Gender gender) {
         return kittens.stream().filter(o -> o.getGender().equals(gender)).collect(Collectors.toList());
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public List<Kitten> findKittensBetweenAges(int minAge, int maxAge) {
 
         return kittens.stream()
@@ -37,17 +70,31 @@ public class KittenStatistics {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public Optional<Kitten> findFirstKittenWithGivenName(String givenName) {
 
         return kittens.stream().filter(i -> i.getName().toUpperCase().equals(givenName.toUpperCase())).findFirst();
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public List<Kitten> kittensSortedByAgeYoungerFirst() {
         return kittens.stream().sorted(Comparator.comparingInt(Kitten::getAge)).collect(Collectors.toList());
     }
 
+    /**
+     * something
+     *
+     * @return something
+     */
     public List<Kitten> kittensSortedByAgeOlderFirst() {
         return kittens.stream().sorted(Comparator.comparingInt(Kitten::getAge).reversed()).collect(Collectors.toList());
     }
-
 }
